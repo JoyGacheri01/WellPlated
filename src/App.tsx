5965b6a39860fb13dc8bb5d085821c012f312903
JoyGacheri01 <joydouglas011@gmail.com>
2025-11-06 13:51:23 +0300

    Day1 SetUp

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-600">Nutrient Track!</h1>
+      <p className="text-gray-600">Track your recipies and colories with ease!</p>
+    </div>
+  );
+}
+
+export default App;
